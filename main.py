4b0691cbd9926591145abd4ff88ad19c78669519
Nilay Nagar <45895008+nilzzzz19@users.noreply.github.com>
2022-06-16 02:01:28 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+np.warnings.filterwarnings("ignore")
+
+
+def complex_matrix(xmin, xmax, ymin, ymax, pixel_density):
+    re = np.linspace(xmin, xmax, int((xmax - xmin) * pixel_density))
+    im = np.linspace(ymin, ymax, int((ymax - ymin) * pixel_density))
+    return re[np.newaxis, :] + im[:, np.newaxis] * 1j
+
+
+def is_stable(c, num_iterations):
+    z = 0
+    for _ in range(num_iterations):
+        z = z ** 2 + c
+    return abs(z) <= 2
+
+
+def get_members(c, num_iterations):
+    mask = is_stable(c, num_iterations)
+    return c[mask]
+
+
+c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=21)
+members = get_members(c, num_iterations=20)
+
+plt.scatter(members.real, members.imag, color="red", marker=",", s=1)
+plt.gca().set_aspect("equal")
+plt.axis("off")
+plt.tight_layout()
+plt.show()
+
+c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=512)
+plt.imshow(is_stable(c, num_iterations=20), cmap="binary")
+plt.gca().set_aspect("equal")
+plt.axis("off")
+plt.tight_layout()
+plt.show()
+
+
+c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=512)
+image = Image.fromarray(~is_stable(c, num_iterations=20))  #Pillow assumes a black background by default.
+image.show()
+
